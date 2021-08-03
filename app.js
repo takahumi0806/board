e69b01d5ae3af9d3a5710fe61d4d742ed62d9419
takahumi0806 <yabuta0439@hotmail.com>
2021-08-04 00:29:58 +0900

    app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+var express = require("express");
+var app = express();
+
+/* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
+var server = app.listen(3000, function(){
+    console.log("Node.js is listening to PORT:" + server.address().port);
+});
