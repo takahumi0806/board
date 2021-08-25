a8563fc8ecae189d6951b7cd5b0b90a2e50d4e85
takahumi0806 <yabuta0439@hotmail.com>
2021-08-25 16:35:57 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const passport = require('passport');
-const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 app.use(express.static('public'));
